4c1d434fb0e3dc0a48d1bfe90f8828ef2061e888
jclaytor2010 <jclaytor@uwyo.edu>
2017-09-18 19:26:28 -0600

    node.cpp file added.

---
+++ Lab2/node.cpp
@@ -0,0 +1,31 @@
+// Node.cpp
+// Jacob Claytor
+// September 18, 2017
+// Definitions of the Node class methods
+
+
+#include "Node.h"
+
+
+Node::Node(double entry)
+	: entry_(entry), next_(NULL)
+{
+	cout << "Node( " << entry_ << ", " << next_ <<
+		" ) created at " << this << endl;
+}
+
+Node::Node(double entry, Node * next)
+	: entry_(entry), next_(next)
+{
+	cout << "Node( " << entry_ << ", " << next_ <<
+		" ) created at " << this << endl;
+}
+
+Node::~Node()
+{
+	cout << "Node( " << entry_ << ", " << next_ <<
+		" ) at " << this << " destroyed" << endl;
+}
+
+
+
